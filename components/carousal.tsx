5a43699238e89245211ea4101cd503a70fb39945
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-06-14 12:55:31 +0530

    Fix : Carousel

--- components/carousal.tsx
+++ components/carousal.tsx
@@ -2,39 +2,56 @@
 import React, { useState } from "react"
 import gsap from "gsap"
 
-export default function carousal() {
-  let ctx=gsap.context()
-  const sliderImageRef = React.useRef(null);
-  
-  const [sliderIndex, setSliderIndex] = useState(0);
-  const sliders = [
-    {
-      url: '/next.svg'
-    },
-    {
-      url: '/vercel.svg'
-    },
-    {
-      url: '/next.svg'
-    },
-    {
-      url: '/vercel.svg'
-    }
-  ]
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-  React.useEffect(() =>{
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-    setTimeout(() => {
-      gsap.to(sliderImageRef.current, {opacity: 0, duration: 1, delay: 3})
-    },3000)
-    setTimeout(() => {
-      setSliderIndex((sliderIndex+1)%4);
-      gsap.to(sliderImageRef.current, {opacity: 1, duration: 3})
-    },7000)
-  })
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function Carousal() {
   return (
-    <div>
-      <img ref={sliderImageRef} src={sliders[sliderIndex].url} alt="" className="ease-in-out"/>
+    <div className="flex h-[30vh] object-contain" >
+      <Swiper
+                pagination={{
+                  clickable: true,
+              }}
+              modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+
+              }}
+              navigation={true}
+              
+              
+            >
+                <SwiperSlide>
+                    <img
+                        className="object-contain "
+                        src="https://cdn.pixabay.com/photo/2022/03/20/15/40/nature-7081138__340.jpg"
+                        alt="image slide 1"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                          className="object-contain w-full "
+                        src="https://cdn.pixabay.com/photo/2022/07/24/17/55/wind-energy-7342177__340.jpg"
+                        alt="image slide 2"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                          className="object-contain w-full "
+                        src="https://cdn.pixabay.com/photo/2022/07/26/03/35/jogger-7344979__340.jpg"
+                        alt="image slide 3"
+                    />
+                </SwiperSlide>
+            </Swiper>
     </div>
   )
 }
