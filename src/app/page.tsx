7260f90dab2be103ae4bf8a02f859b4879d06f7a
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-06-23 19:26:22 +0530

    Update : pag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Carousal from "../../components/carousal";
 
 export default function Home() {
   return (
-    <main className="  ">
+    <main className=" w-[90%] mx-auto ">
       <nav className="w-full p-10 flex flex-row justify-between bg-slate-100">
         <div className="text-2xl font-semibold">Blossom Flora</div>
         <div className="flex flex-row gap-7">
@@ -14,8 +14,13 @@ export default function Home() {
         </div>
       </nav>
 
-      <section id="landing" className="bg-red-400 min-w-full h-[100%]">
+      <section id="landing" className=" min-w-full h-[100%]">
         <Carousal/>
+
+
+        <div className="flex items-center">
+          
+        </div>
       </section>
     </main>
   );
