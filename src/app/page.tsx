853ea54bed7131f13230ff6ccab758df12d915d1
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-06-26 21:41:24 +0530

    feat : About Us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,21 @@ export default function Home() {
           Blossom Flora
         </div>
         <div className="flex w-full text-2xl font-thin items-center text-center justify-center gap-4 mt-3 ">
-          <div className="hover:font-medium hover:cursor-pointer transition-all">HOME</div>
-          <div className="hover:font-medium hover:cursor-pointer transition-all">SHOP</div>
-          <div className="hover:font-medium hover:cursor-pointer transition-all">SERVICES</div>
-          <div className="hover:font-medium hover:cursor-pointer transition-all">CONTACT US</div>
+          <div className="hover:font-medium hover:cursor-pointer transition-all">
+            HOME
+          </div>
+          <div className="hover:font-medium hover:cursor-pointer transition-all">
+            SHOP
+          </div>
+          <div className="hover:font-medium hover:cursor-pointer transition-all">
+            SERVICES
+          </div>
+          <div className="hover:font-medium hover:cursor-pointer transition-all">
+            CONTACT US
+          </div>
         </div>
       </nav>
       <Slider />
-
 
       <section id="catalog" className="mt-12">
         <div className="flex flex-col items-center">
@@ -48,10 +55,23 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section id="about" className="w-full">
-        <div>dhf</div>
+      <section id="about" className="w-full flex">
         <div className="flex justify-center w-[50%] h-full items-center">
-          <Thumbnails/>
+          <Thumbnails />
+        </div>
+        <div className="flex flex-col items-center w-[50%]">
+          <div className="my-[50px] text-black text-6xl">About Us</div>
+          <div className=" text-xl font-light items-center w-[80%] mx-auto">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </div>
         </div>
       </section>
     </main>
